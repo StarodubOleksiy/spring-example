804685c7257b45b0b93b24175e6840393ac7ede0
StarodubOleksiy <StarodubAleks@gmail.com>
2016-05-01 23:38:21 +0300

    fourth commit

--- src/main/java/ua/goit/java/Main.java
+++ src/main/java/ua/goit/java/Main.java
@@ -71,7 +71,7 @@ public class Main {
         }
     }
 
-
+//
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
        Main main = applicationContext.getBean("main",Main.class);
